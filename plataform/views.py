4ba1ab280add7254ba92e357cab76b61e44d9cc1
Vítor Lobo <vitorlobof@gmail.com>
2023-07-10 12:38:53 -0300

    A list of the users was added to the home page.

--- plataform/views.py
+++ plataform/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
+from users.models import User
 
 @login_required(login_url='login')
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {
+        'users': User.objects.all()
+    })
 
